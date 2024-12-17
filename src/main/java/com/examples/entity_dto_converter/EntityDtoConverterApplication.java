ec6701ed95aa63877dc70a619a430805616e6d15
Angélica Martins <marttinsangelica@gmail.com>
2024-12-17 16:54:42 -0300

    Feature: Initial commit

---
+++ src/main/java/com/examples/entity_dto_converter/EntityDtoConverterApplication.java
@@ -0,0 +1,13 @@
+package com.examples.entity_dto_converter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntityDtoConverterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EntityDtoConverterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/examples/entity_dto_converter/EntityDtoConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.examples.entity_dto_converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntityDtoConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
