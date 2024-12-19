bb592e01d51190eefb9a656e9521d3cbaa35b753
Angélica Martins <marttinsangelica@gmail.com>
2024-12-19 18:57:45 -0300

    Feature: Add saveUserEntityManualMapper and saveUserDtoManualMapper

--- src/main/java/com/examples/entity_dto_converter/controller/UserController.java
+++ src/main/java/com/examples/entity_dto_converter/controller/UserController.java
@@ -1,9 +1,11 @@
 package com.examples.entity_dto_converter.controller;
 
 import com.examples.entity_dto_converter.converters.UserEntityBuilder;
+import com.examples.entity_dto_converter.converters.UserEntityManualMapper;
 import com.examples.entity_dto_converter.converters.UserEntityStaticMethod;
 import com.examples.entity_dto_converter.dto.UserDTOBuilder;
 import com.examples.entity_dto_converter.dto.UserDTOConstructor;
+import com.examples.entity_dto_converter.dto.UserDTOManualMapper;
 import com.examples.entity_dto_converter.dto.UserDTOStaticMethod;
 import com.examples.entity_dto_converter.model.UserEntity;
 import com.examples.entity_dto_converter.repository.UserRepository;
@@ -36,11 +38,11 @@ public class UserController {
     return ResponseEntity.ok(userRepository.save(new UserEntityBuilder().toEntityBuilder(requestUser)));
   }
 
-//  @PostMapping("/dto/manual-mapper")
-//  public ResponseEntity<User> saveUser(@RequestBody UserDTOManualMapper requestUser) {
-//    return ResponseEntity.ok();
-//  }
-//
+  @PostMapping("/dto/manual-mapper")
+  public ResponseEntity<UserEntity> saveUserEntityManualMapper(@RequestBody UserDTOManualMapper requestUser) {
+    return ResponseEntity.ok(new UserEntityManualMapper().toEntityManualMapper(requestUser));
+  }
+
 //  @PostMapping("/dto/mapstruct")
 //  public ResponseEntity<User> saveUser(@RequestBody UserDTOMapStruct requestUser) {
 //    return ResponseEntity.ok();
@@ -74,11 +76,12 @@ public class UserController {
     return ResponseEntity.ok(new UserDTOBuilder().toDto(userEntity));
   }
 
-//  @PostMapping("/entity/manual-mapper")
-//  public ResponseEntity<UserDTOManualMapper> saveUser(@RequestBody User user) {
-//    return ResponseEntity.ok();
-//  }
-//
+  @PostMapping("/entity/manual-mapper")
+  public ResponseEntity<UserDTOManualMapper> saveUserEntityManualMapper(@RequestBody UserEntity userEntity) {
+    userRepository.save(userEntity);
+    return ResponseEntity.ok(new UserDTOManualMapper().toDto(userEntity));
+  }
+
 //  @PostMapping("/entity/mapstruct")
 //  public ResponseEntity<UserDTOMapStruct> saveUser(@RequestBody User user) {
 //    return ResponseEntity.ok();
