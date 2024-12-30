0a35d3485a4eb271627f2372cbc1b7150419d7b3
Angélica Martins <marttinsangelica@gmail.com>
2024-12-30 17:20:43 -0300

    Feature: Add Builder and Mapstruct benchmark

--- src/main/java/com/examples/entitydtoconverter/Bench.java
+++ src/main/java/com/examples/entitydtoconverter/Bench.java
@@ -1,46 +1,62 @@
 package com.examples.entitydtoconverter;
 
+import com.examples.entitydtoconverter.converters.UserMapper;
 import com.examples.entitydtoconverter.dto.UserDtoBuilder;
 import com.examples.entitydtoconverter.model.UserEntity;
-import java.time.LocalDateTime;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
-import lombok.AllArgsConstructor;
+import org.mapstruct.factory.Mappers;
 import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
 import org.openjdk.jmh.annotations.Level;
-import org.openjdk.jmh.annotations.Mode;
 import org.openjdk.jmh.annotations.OutputTimeUnit;
 import org.openjdk.jmh.annotations.Scope;
 import org.openjdk.jmh.annotations.Setup;
 import org.openjdk.jmh.annotations.State;
+import org.openjdk.jmh.infra.Blackhole;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
+import org.openjdk.jmh.runner.options.TimeValue;
 
 @State(Scope.Thread)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 public class Bench {
 
   private UserDtoBuilder userDtoBuilder;
 
+  private UserMapper userMapper;
+
+  private UserEntity userEntity;
+
   @Setup(Level.Iteration)
   public void setup() {
     userDtoBuilder = new UserDtoBuilder();
+    userMapper = Mappers.getMapper(UserMapper.class);
+    userEntity = new UserEntity("Angélica", 33);
   }
 
   @Benchmark
-  @BenchmarkMode(Mode.AverageTime)
-  @OutputTimeUnit(TimeUnit.MICROSECONDS)
-  public void testingBenchmark() {
-    UserEntity userEntity = new UserEntity("Angélica", 33);
-    userDtoBuilder.toDto(userEntity);
+  public void benchmarkBuilderDtoBatch(Blackhole blackhole) {
+    for (int i = 0; i < 1000; i++) {
+      blackhole.consume(userDtoBuilder.toDto(userEntity));
+    }
+  }
+
+  @Benchmark
+  public void benchmarkMapstructDtoBatch(Blackhole blackhole) {
+    for (int i = 0; i < 1000; i++) {
+      blackhole.consume(userMapper.toDto(userEntity));
+    }
   }
 
   public static void main(String[] args) throws RunnerException {
     Options opt = new OptionsBuilder()
       .include(Bench.class.getSimpleName())
-      .forks(1)
+      .forks(2)
+      .warmupIterations(10)
+      .measurementIterations(10)
+      .warmupTime(TimeValue.seconds(1))
+      .measurementTime(TimeValue.seconds(1))
       .build();
 
     new Runner(opt).run();
